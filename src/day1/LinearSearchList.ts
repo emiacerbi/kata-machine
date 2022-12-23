e5075bc316a62e0d5693d950b583abe6a87a5485
Emiliano Acerbi <eacerbip@gmail.com>
2022-12-23 15:21:32 -0300

    linear search

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,12 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (let index = 0; index < haystack.length; index++) {
+        if (haystack[index] === needle) {
+            return true;
+        }
+    }
 
+    return false;
 }
